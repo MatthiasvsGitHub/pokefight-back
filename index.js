156b44d58be036ef279a3737ac6db7e29ce6e358
MatthiasvsGitHub <80052472+MatthiasvsGitHub@users.noreply.github.com>
2021-05-25 15:09:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(port, console.log(`Server is running on port ${port}`));
+app.listen(process.env.PORT || port, console.log(`Server is running on port ${port}`));
